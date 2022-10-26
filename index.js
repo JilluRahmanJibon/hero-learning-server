c61329fe5458df64855381e2be1e290534219534
Jillu Rahman Jibon <jillurahmanjibon@gmail.com>
2022-10-26 15:13:13 +0600

    all data are update

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 1853239027;
+const port = process.env.PORT || 9000;
 
 app.use(cors());
-
+app.get("/", (req, res) => {
+	res.send("Now ready for use");
+});
 app.listen(port, () => {
 	console.log("Port is running now: ", port);
+});
+
+const categories = require("./data/Categories");
+
+app.get("/course_categories", (req, res) => {
+	res.send(categories);
 });
