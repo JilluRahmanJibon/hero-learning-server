63c062e30dfa35fbf14fbbba3cbe145aca516cae
Jillu Rahman Jibon <jillurahmanjibon@gmail.com>
2022-12-04 11:42:28 +0600

    author name added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 9000;
-
+// author Jillu Rahman Jibon 
 app.use(cors());
 app.get("/", (req, res) => {
 	res.send("Now ready for use");
